d4b343adcdafcc5143423d43dd1902efe065273f
TequilaSunset <nrz6360666@163.com>
2023-08-28 17:36:18 +0800

    使用redis替代session保存验证码，新增token更新拦截器

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -12,10 +14,17 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -29,6 +38,8 @@ import java.util.Random;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //1.手机号校验
@@ -39,8 +50,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         }
         //3.符合，生成验证码
         String code= RandomUtil.randomNumbers(6);
-        //4.保存验证码到session
-        session.setAttribute("code",code);
+        //4.保存验证码到session->保存到redis中  set key value ex 120
+//        session.setAttribute("code",code);
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
         //5.发送验证码
         log.debug("发送短信验证码成功，验证码：{}",code);
         //6.返回ok
@@ -57,9 +69,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         }
         //2.校验验证码
-        Object cacheCode = session.getAttribute("code");
+//        Object cacheCode = session.getAttribute("code");
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
         String code=loginForm.getCode();
-        if(cacheCode==null || !cacheCode.toString().equals(code)){
+        if(cacheCode==null || !cacheCode.equals(code)){
             //3.如果不一致，报错
             return Result.fail("验证码错误");
         }
@@ -71,9 +84,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             //6.如果不存在，创建新用户并保存
             user=createUserWithPhone(phone);
         }
-        //7。如果存在，保存用户信息到session中。
-        session.setAttribute("user", BeanUtil.copyProperties(user,UserDTO.class));
-        return Result.ok();
+        //7。如果存在，保存用户信息到session中->redis中。
+        //7.1 随机生成token，作为登录令牌
+        String token = UUID.randomUUID().toString(true);
+        //7.2将user转为HashMap存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName,fieldValue)->fieldValue.toString()));
+        //7.3 存储
+        stringRedisTemplate.opsForHash().putAll(LOGIN_USER_KEY+token,userMap);
+        stringRedisTemplate.expire(LOGIN_USER_KEY+token,LOGIN_USER_TTL,TimeUnit.MINUTES);
+        //8 返回token
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,34 +1,32 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //1.获取session
-        HttpSession session = request.getSession();
-        //2.获取session中的用户
-        Object user = session.getAttribute("user");
-        //3.判断用户是否存在
-        if(user==null){
-            //4.如果不存在，拦截
+        //判断threadlocal中有没有用户，没有就拦截
+        if (UserHolder.getUser()==null) {
+            //没有，设置状态码
             response.setStatus(401);
+            //拦截
             return false;
         }
-        //5.如果存在，保存用户信息到threadlocal中
-        UserHolder.saveUser((UserDTO)user);
-        //6.放行
+        //有用户，放行
         return true;
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
-    }
 }
